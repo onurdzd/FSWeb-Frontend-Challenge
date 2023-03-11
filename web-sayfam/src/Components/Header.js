07fa8d3197f5bb5bcb4f447316f5c955c0280e27
onurd <106928484+onurdzd@users.noreply.github.com>
2023-03-11 12:49:34 +0300

    profile css eklendi

--- web-sayfam/src/Components/Header.js
+++ web-sayfam/src/Components/Header.js
@@ -1,28 +1,34 @@
 import React from "react";
-import imgHero from "../images/hero-right.png"
-import github from "../images/github.svg"
-import linkedin from "../images/LinkedIn.svg"
+import imgHero from "../images/hero-right.jpg";
+import github from "../images/github.svg";
+import linkedin from "../images/LinkedIn.svg";
 
 const Header = () => {
   return (
     <div className="header ">
       <div className="header-box-1">
+        <h2>Onur Erk</h2>
         <div className="header-box-1-text">
-          <h2>Onur Erk</h2>
           <h1>I am a Frontend Developer...</h1>
           <p>
             ...who likes to craft solid and scalable frontend products with
             great user experiences.
           </p>
           <div className="header-box-1-button">
-          <button><img src={github} alt=""></img><span>Github</span></button>
-          <button><img src={linkedin} alt=""></img><span>Linkedin</span></button>
+            <button>
+              <img src={github} alt=""></img>
+              <span>Github</span>
+            </button>
+            <button>
+              <img src={linkedin} alt=""></img>
+              <span>Linkedin</span>
+            </button>
           </div>
         </div>
       </div>
       <div className="header-box-2">
         <div className="header-box-2-image">
-        <img alt="hero-right" src={imgHero}></img>
+          <img alt="hero-right" src={imgHero}></img>
         </div>
       </div>
     </div>
--- web-sayfam/src/Components/Profile.js
+++ web-sayfam/src/Components/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import img from "../images/profile_img.png";
+import img from "../images/profile_img.jpg";
 
 const Profile = () => {
   return (
@@ -7,23 +7,25 @@ const Profile = () => {
       <div className="container">
         <h1>Profile</h1>
         <div className="profile-box">
-          <div>
+          <div className="profile-box-1 profile-box-container">
             <h2>Basic Information</h2>
-            <div><p>Doğum Tarihi</p><span>03.04.1991</span></div>
-            <div><p>İkamet Şehri</p><span>Antalya</span></div>
-            <div><p>Eğitim Durumu</p><span>Ege Üniversitesi Finans Yönetimi Yüksek Lisans</span></div>
-            <div><p>Tercih Ettiği Rol</p><span>Full Stack</span></div>
+            <div className="profile-box-1-detay"><p>Doğum Tarihi</p><span>03.04.1991</span></div>
+            <div className="profile-box-1-detay"><p>İkamet Şehri</p><span>Antalya</span></div>
+            <div className="profile-box-1-detay"><p>Eğitim Durumu</p><span>Ege Üniversitesi Finans Yönetimi Yüksek Lisans 2017</span></div>
+            <div className="profile-box-1-detay"><p>Tercih Ettiği Rol</p><span>Full Stack</span></div>
           </div>
-          <div>
+          <div className="profile-box-2 profile-box-container">
             <img src={img} alt="profile_pic"></img>
           </div>
-          <div>
+          <div className="profile-box-3 profile-box-container">
             <h2>About Me</h2>
             <p>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
-              aut, odit laborum aliquam voluptatum nisi mollitia. Mnima
+              aut, odit laborum aliquam voluptatum nisi mollitia.
+              <br></br><br></br>
+              Mnima
               accusamus ratione soluta aperiam sit voluptate? Dicta quod
-              deserunt quam temporibus cumque magnam!{" "}
+              {" "}
             </p>
           </div>
         </div>
