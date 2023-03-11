5c6e8e138f45e102125869f79fa250c518e607a9
onurd <106928484+onurdzd@users.noreply.github.com>
2023-03-11 13:19:24 +0300

    project component başlandı

--- web-sayfam/src/Components/Projects.js
+++ web-sayfam/src/Components/Projects.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const Projects = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="projects">
+      <div className="container">
+        <h1>Projects</h1>
+        <div className="projects-box-1">
+          <div><img alt="project-img"></img></div>
+          <div></div>
         </div>
-    );
+        <div className="projects-box-2">
+          <div><img alt="project-img"></img></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
