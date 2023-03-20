9c36a4d8be2b6df9845eb9e07f731c6b4506793a
onurd <106928484+onurdzd@users.noreply.github.com>
2023-03-20 11:48:30 +0300

    header button güncelleme

--- web-sayfam/src/Components/Header.js
+++ web-sayfam/src/Components/Header.js
@@ -1,42 +1,46 @@
 import React from "react";
 import imgHero from "../images/hero-right.jpg";
-import github from "../images/github.svg";
-import linkedin from "../images/LinkedIn.svg";
+import github from "../images/github.png";
+import linkedin from "../images/linkedin.png";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="header">
       <div className="header-box container">
-      <div className="header-box-1 ">
-        <div className="header-box-1-text">
-          <h2>Onur Erk</h2>
-          <div>
-            <h1>I am a Frontend Developer...</h1>
-            <p>
-              ...who likes to craft solid and scalable frontend products with
-              great user experiences.
-            </p>
-            <div className="header-box-1-button">
-              <div>
-                <button className="header-box-1-button">
-                  <img src={github} alt=""></img>
-                 <a href="https://github.com/onurdzd">Github</a>
-                </button>
-              </div>
-              <div>
-                <button className="header-box-1-button"><img src={linkedin} alt=""></img>
-                <a href="https://www.linkedin.com/feed/">Linkedin
-                 </a>
-                </button>
+        <div className="header-box-1 ">
+          <div className="header-box-1-text">
+            <h2>Onur Erk</h2>
+            <div>
+              <h1>I am a Frontend Developer...</h1>
+              <p>
+                ...who likes to craft solid and scalable frontend products with
+                great user experiences.
+              </p>
+              <div className="header-box-1-button">
+                  <button
+                    onClick={() =>
+                      window.open("https://github.com/onurdzd", "_blank")
+                    }
+                  >
+                    <img src={github} alt=""></img>
+                    Github
+                  </button>
+                  <button
+                    onClick={() =>
+                      window.open("https://www.linkedin.com/feed/", "_blank")
+                    }
+                  >
+                    <img src={linkedin} alt=""></img>
+                   Linkedin
+                  </button>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="header-box-2">
-        <img alt="hero-right" src={imgHero}></img>
-      </div>
+        <div className="header-box-2">
+          <img alt="hero-right" src={imgHero}></img>
+        </div>
       </div>
     </div>
   );
