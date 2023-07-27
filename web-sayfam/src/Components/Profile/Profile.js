6802a7a983d838f1d22c285e0d2760961ff5f772
onurd <106928484+onurdzd@users.noreply.github.com>
2023-07-27 16:12:06 +0300

    profile p updated

--- web-sayfam/src/Components/Profile/Profile.js
+++ web-sayfam/src/Components/Profile/Profile.js
@@ -30,12 +30,13 @@ const Profile = () => {
           <div className="profile-box-2 profile-box-container">
             <img src={img} alt="profile_pic"></img>
           </div>
-          <div className="profile-box-3 profile-box-container">
+          <div className="profile-box-3 profile-box-container-2">
             <p>
               I have worked in the financial sector for almost 7 years. Due to
               my personal interest in programming, I decided to go into
               development.</p> <br></br><p>
-              I am already completed full stack web development course at Workintech company succesfully.Also I have attended some courses about Javascript,HTML,CSS,SQL,.NET,C#,Java,Object Oriented.
+              I am already completed full stack web development course at Workintech company succesfully.
+              Also I have attended some courses about Javascript, HTML, CSS, SQL, .NET, C#, Java, Object Oriented.
               I am willing to work and improve myself in both frontend and backend development.
             </p>
           </div>
